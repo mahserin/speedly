6b9350ca8c7b57116a64fdba1dc911fbecefc7e9
mahserin <mahserin021@gmail.com>
2025-09-30 12:35:52 +0330

    feat: Integrate cosmiconfig for configuration management and update package dependencies

--- src/util/getConfig.ts
+++ src/util/getConfig.ts
@@ -1,16 +1,12 @@
 import fs from 'fs'
 // const { path } = require('../../app');
-export default (configField : string) : {[key : string] : unknown} => {
-    const stackPath = new Error().stack?.split('\n')[3].trim().match(/\(.+\)/)?.[0].replace(/([\(\)]|\:\d+\:\d+)/g, '');
-    for(let i = 1 ; i < 4 ; i++) {
-        const filePath = stackPath?.replace(/\\/g, '/').split('/').slice(0, -i).join('/') + '/speedly.config.js';
-        if(fs.existsSync(filePath)) {
-            const config = require(filePath);
-            if (config && config[configField]) {
-                return config[configField];
-            }
-            return {}
-            }
-        }
-        return {};
+import { cosmiconfigSync } from 'cosmiconfig';
+export default (configField: string): { [key: string]: unknown } => {
+    const explorer = cosmiconfigSync("speedly"); // اسم پروژه‌ت
+    const result = explorer.search();
+
+    if (result && result.config && result.config[configField]) {
+        return result.config[configField];
     }
+    else return {}
+}
