2331e7d433a30f8a4f8f3c97df5710730a42c97f
mahserin <mahserin021@gmail.com>
2025-09-30 17:28:05 +0330

    modified:   package.json 	modified:   src/util/getConfig.ts

--- src/util/getConfig.ts
+++ src/util/getConfig.ts
@@ -3,7 +3,7 @@ import fs from 'fs'
 import { cosmiconfigSync } from 'cosmiconfig';
 export default (configField: string): { [key: string]: unknown } => {
     const explorer = cosmiconfigSync("speedly"); // اسم پروژه‌ت
-    const result = explorer.search(process.cwd());
+    const result = explorer.search();
 
     if (result && result.config && result.config[configField]) {
         return result.config[configField];
