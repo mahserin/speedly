9aa8b12127024916c7928ad8308efd2cd3485e5e
mahserin <mahserin021@gmail.com>
2025-09-30 18:47:20 +0330

    Refactor config retrieval logic to use dynamic file searching

--- src/util/getConfig.ts
+++ src/util/getConfig.ts
@@ -1,12 +1,32 @@
 import fs from 'fs'
-// const { path } = require('../../app');
-import { cosmiconfigSync } from 'cosmiconfig';
-export default (configField: string): { [key: string]: unknown } => {
-    const explorer = cosmiconfigSync("speedly"); // اسم پروژه‌ت
-    const result = explorer.search(process.cwd());
-
-    if (result && result.config && result.config[configField]) {
-        return result.config[configField];
-    }
-    else return {}
+import path from 'path';
+const find = (field : string , ...args : string[])  => {
+  const rel = path.join.apply(null, [].slice.call(args));
+  if(!require?.main?.filename)return {}
+  return findStartingWith(path.dirname(require.main.filename), rel,field);
 }
+
+const findStartingWith  = (start : string, rel : string , field : string) => {
+  const file = path.join(start, rel);
+  const formats = ['.ts','.js' , '.json']
+  for(const [index , format] of formats.entries()){
+
+      try {
+    if(fs.statSync(file + format)) return require(file + format)[field] || {}      
+    } catch (err) {
+        if(index + 1 != formats.length) continue
+        if (path.dirname(start) !== start) {
+            return findStartingWith(path.dirname(start), rel,field);
+        }
+        return {}
+    }
+}
+}
+const configGetter =  (configField : string) : {[key : string] : unknown} => {
+    const foundData = find(configField , 'speedly.config')
+    if(foundData) return foundData
+    return {}
+}
+
+
+export default configGetter
