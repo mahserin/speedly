5a685b82abb17dd10b620600dcadc4abf7e46c33
mahserin <mahserin021@gmail.com>
2025-09-30 12:01:16 +0330

    fix: Correct stack trace index in getConfig function to accurately locate configuration file

--- src/util/getConfig.ts
+++ src/util/getConfig.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 // const { path } = require('../../app');
 export default (configField : string) : {[key : string] : unknown} => {
-    const stackPath = new Error().stack?.split('\n')[2].trim().match(/\(.+\)/)?.[0].replace(/([\(\)]|\:\d+\:\d+)/g, '');
+    const stackPath = new Error().stack?.split('\n')[3].trim().match(/\(.+\)/)?.[0].replace(/([\(\)]|\:\d+\:\d+)/g, '');
     for(let i = 1 ; i < 4 ; i++) {
         const filePath = stackPath?.replace(/\\/g, '/').split('/').slice(0, -i).join('/') + '/speedly.config.js';
         if(fs.existsSync(filePath)) {
