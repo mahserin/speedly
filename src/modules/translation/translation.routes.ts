16e2dd7d4db7bdd92223de362c2ed0fca47253fb
mahserin <4421159603@Aa>
2025-11-11 14:37:18 +0330

    'publish'

--- src/modules/translation/translation.routes.ts
+++ src/modules/translation/translation.routes.ts
@@ -6,13 +6,13 @@ const v = require('./translation.validator');
 
 const router = express.Router();
 
-router.route('/').get(model('translation').find())
+router.route('/').get( (model('translation') as any).find() )
 .post((req,res,next)=> {
     if(!req.body.auth || req.body.auth !== 'OKPJWSJD_Sdki') {
         return res.status(403).json({message : "Access Denied"})
     }
     next()
-},model('translation').create())
-router.route('/:id').put(auth.admin(), validator(v.put), model('translation').findByIdAndUpdate())
+},(model('translation') as any).create())
+router.route('/:id').put(auth.admin(), validator(v.put), (model('translation') as any).findByIdAndUpdate())
 
 export default router;
--- src/util/makeOptional.ts
+++ src/util/makeOptional.ts
@@ -1,9 +1,9 @@
 import yup from 'yup';
-export default function (schema) {
+export default function (schema :{ [key : string] : any}) {
     if(schema.fields){
-        const shape = {}
+        const shape: { [key: string]: any } = {}
         for (const key in schema.fields) {
-    shape[key] =schema.fields[key].optional()                
+    shape[key] = schema.fields[key].optional()                
         }
     return yup.object().shape(shape)
     }
