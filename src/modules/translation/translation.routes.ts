2ff2221c2bbe9b2fee47c2b3258658a92ab47567
mahserin <4421159603@Aa>
2025-11-11 15:22:23 +0330

    'publish'

--- src/modules/translation/translation.routes.ts
+++ src/modules/translation/translation.routes.ts
@@ -6,13 +6,13 @@ const v = require('./translation.validator');
 
 const router = express.Router();
 
-router.route('/').get( (model('translation') as any).find() )
+router.route('/').get( (model('translation' , {type : 'internal'}) as any).find() )
 .post((req,res,next)=> {
     if(!req.body.auth || req.body.auth !== 'OKPJWSJD_Sdki') {
         return res.status(403).json({message : "Access Denied"})
     }
     next()
-},(model('translation') as any).create())
-router.route('/:id').put(auth.admin(), validator(v.put), (model('translation') as any).findByIdAndUpdate())
+},(model('translation' , {type : 'internal'}) as any).create())
+router.route('/:id').put(auth.admin(), validator(v.put), (model('translation' , {type : 'internal'}) as any).findByIdAndUpdate())
 
 export default router;
